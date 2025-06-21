8f53290cf66b5688f785b0396cd2358d7a07bf76
jared <jaredrotich3@gmail.com>
2025-06-21 12:11:13 +0300

    adjusted the client and server

--- server/app.py
+++ server/app.py
@@ -17,57 +17,41 @@ db.init_app(app)
 @app.route('/messages', methods=['GET', 'POST'])
 def messages():
     if request.method == 'GET':
-        messages = Message.query.order_by('created_at').all()
-
-        response = make_response(
-            jsonify([message.to_dict() for message in messages]),
-            200,
+       messages = Message.query.order_by(Message.created_at).all()
+       return make_response(
+            jsonify([message.to_dict() for message in messages]), 200 
         )
     
     elif request.method == 'POST':
         data = request.get_json()
-        message = Message(
-            body=data['body'],
-            username=data['username']
+        new_message = Message(
+            body=data.get("body"),
+            username=data.get("username")
         )
 
-        db.session.add(message)
+        db.session.add(new_message)
         db.session.commit()
 
-        response = make_response(
-            jsonify(message.to_dict()),
-            201,
-        )
-
-    return response
+    return make_response(jsonify(new_message.to_dict()), 201)
 
 @app.route('/messages/<int:id>', methods=['PATCH', 'DELETE'])
 def messages_by_id(id):
-    message = Message.query.filter_by(id=id).first()
-
+    message = Message.query.get_or_404(id)
     if request.method == 'PATCH':
         data = request.get_json()
         for attr in data:
             setattr(message, attr, data[attr])
             
-        db.session.add(message)
+        
         db.session.commit()
 
-        response = make_response(
-            jsonify(message.to_dict()),
-            200,
-        )
+        return make_response(jsonify(message.to_dict()), 200)
 
     elif request.method == 'DELETE':
         db.session.delete(message)
         db.session.commit()
 
-        response = make_response(
-            jsonify({'deleted': True}),
-            200,
-        )
-
-    return response
+        return make_response(jsonify({'deleted': True}), 200)
 
 if __name__ == "__main__":
     app.run(port=5555)
